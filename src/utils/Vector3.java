62347fa40bc1384bed42655d6f8891043f36f344
Sami Pasquali <sami.pas@gmx.at>
2020-04-03 16:56:09 +0200

    further code

--- src/utils/Vector3.java
+++ src/utils/Vector3.java
@@ -12,7 +12,31 @@ public class Vector3 {
     }
 
     public Vector3(String lat, String lo, String alt) {
+        double latAngle = Double.parseDouble(lat);
+        double loAngle = Double.parseDouble(lo);
+        double altDouble = Double.parseDouble(alt);
 
+        Vector3 base = new Vector3(1,0,0);
+
+        Matrix3x3 rotY = Matrix3x3.rotationY(latAngle);
+        Matrix3x3 rotZ = Matrix3x3.rotationZ(loAngle);
+
+        base = rotY.mult(base);
+        base = rotZ.mult(base);
+
+        altDouble += (6371 * 1000);
+
+        base.mult(altDouble);
+
+        this.x = base.x;
+        this.y = base.y;
+        this.z = base.z;
+    }
+
+    public void mult(double times) {
+        this.x *= times;
+        this.y *= times;
+        this.y *= times;
     }
 
     public void rotate(double x, double y, double z) {
